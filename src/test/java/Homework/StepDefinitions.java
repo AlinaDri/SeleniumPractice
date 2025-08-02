6c89e2fb9cf90d5e06d30da9954a225cf34bb431
Alina Driksmane-Lurina <driksmane@gmail.com>
2025-08-02 12:47:28 +0300

    compledted first scenario

--- src/test/java/Homework/StepDefinitions.java
+++ src/test/java/Homework/StepDefinitions.java
@@ -7,21 +7,27 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 
 public class StepDefinitions {
 
     private WebDriver driver;
+    private WebDriverWait wait;
+    private String adText; 
 
     @Before
     public void setup() {
         WebDriverManager.chromedriver().clearDriverCache().setup();
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10); // Use seconds as int instead of Duration
     }
 
     @After
@@ -47,6 +53,8 @@ public class StepDefinitions {
         assertThat(driver.findElement(By.xpath("//*[@title='Animals']")).isDisplayed()).isTrue();
         assertThat(driver.findElement(By.xpath("//*[@title='Agriculture']")).isDisplayed()).isTrue();
         assertThat(driver.findElement(By.xpath("//*[@title='Rest, hobbies']")).isDisplayed()).isTrue();
+        WebElement cookieButton = driver.findElement(By.xpath("//button[text()='Accept and continue']"));
+        cookieButton.click();
     }
 
     @When("user navigates to category {string}")
@@ -66,20 +74,33 @@ public class StepDefinitions {
         select.selectByVisibleText("80");
         assertThat(driver.getCurrentUrl()).contains("80");
         WebElement firstAdLink = driver.findElement(By.xpath("//table//tr[td//a[contains(@href, '/msg/')]][1]/td[3]//a"));
-        String adText = firstAdLink.getText();
+        adText = firstAdLink.getText(); // Remove String declaration
         firstAdLink.click();
-        assertThat(driver.getCurrentUrl()).contains("msg/en/transport/cars/audi/80/");
-        assertThat(driver.findElement(By.xpath("//*[@id='msg_div_msg']")).getText()).contains(adText);
+        assertThat(driver.findElement(By.xpath("//*[@id='msg_div_msg']")).getText().replaceAll("[\\r\\n]", " ")).contains(adText);
     }
 
     @When("ads it to Favorites")
     public void user_adds_ad_to_favorites() {
-
+        WebElement favoriteLink = driver.findElement(By.id("add-to-favorites-lnk"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(favoriteLink).click().perform();
+        WebElement alertAccept = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='alert_ok']")));
+        alertAccept.click();
+        WebElement favoritesCounter = driver.findElement(By.id("mnu_fav_id"));
+        actions.moveToElement(favoritesCounter);
+        assertThat(favoritesCounter.getText()).isEqualTo(" (1)");
     }
 
     @Then("ad is successfully added to Favorites")
     public void ad_is_successfully_added_to_favorites() {
-
+        WebElement favoritesTab = driver.findElement(By.xpath("//*[@title='Favorites']"));
+        favoritesTab.click();
+        assertThat(driver.getCurrentUrl()).contains("favorites");
+        WebElement favoriteRow = driver.findElement(By.xpath("//tr[contains(@id, 'tr_')]"));
+        WebElement adTextInFavorites = favoriteRow.findElement(By.xpath(".//td[@class='msg2']//div[@class='d1']//a[@class='am']"));
+        assertThat(adTextInFavorites.isDisplayed()).isTrue();
+        String favoritesAdText = adTextInFavorites.getText();
+        assertThat(favoritesAdText.trim()).contains(adText.trim());
     }
 
 }
